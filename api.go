0e6db68800f52e2424c02824345333ad4f899fc4
Caleb Smith <karagenit@outlook.com>
2024-07-05 23:52:20 -0400

    Fetch Story Titles

--- api.go
+++ api.go
@@ -6,29 +6,9 @@ import (
 	"io"
     "github.com/gin-gonic/gin"
 	"encoding/json"
+	"strings"
+	"strconv"
 )
-
-// album represents data about a record album.
-type album struct {
-    ID     string  `json:"id"`
-    Title  string  `json:"title"`
-    Artist string  `json:"artist"`
-    Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-// getAlbums responds with the list of all albums as JSON.
-func getAlbums(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, albums)
-}
-
-// ------------------
 
 type story struct {
 	ID     int    `json:"id"`
@@ -75,8 +55,26 @@ func updateTopStoryDetails() {
 
 // title, domain, comment_link
 func getStoryDetails(id int) story {
-	return story{
-		ID: id, Title: "test", Domain: "test", Link: "test",
+	var result map[string]interface{}
+
+	url := []string{"https://hacker-news.firebaseio.com/v0/item/", strconv.Itoa(id), ".json"} 
+  
+	resp, _ := http.Get(strings.Join(url, ""))
+	// TODO handle error
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	// TODO handle error
+	_ = json.Unmarshal(body, &result)
+	// TODO handle error
+
+	title, title_ok := result["title"].(string)
+
+	if title_ok {
+		return story{
+			ID: id, Title: title, Domain: "test", Link: "test",
+		}
+	} else {
+		return story{}
 	}
 }
 

0789f9a57a42b4971b98843c71dd264e1bfab197
Caleb Smith <karagenit@outlook.com>
2025-05-13 20:50:28 -0400

    Add Uncategorized Option

--- assets/js/categorySelector.js
+++ assets/js/categorySelector.js
@@ -3,14 +3,21 @@ import categoryStore from "./categoryStore.js";
 // Would be nice if we could just extend the select element but apparently not all browsers support that
 class CategorySelector extends HTMLElement {
     connectedCallback() {
+        // Turn the list of strings into a list of { 'category': 'category' } objs
+        let categories = categoryStore.getCategories();
+        categories = categories.map(
+            (category) => ({ label: category, value: category })
+        );
+        categories.unshift({ label: 'Uncategorized', value: '' })
+        
         const select = document.createElement('select');
-        // todo uncategorized option too
-        categoryStore.getCategories().forEach(category => {
+        categories.forEach(({ label, value }) => {
             const option = document.createElement('option');
-            option.value = category;
-            option.textContent = category;
+            option.value = value;
+            option.textContent = label;
             select.appendChild(option);
         });
+
         this.appendChild(select);
     }
 
